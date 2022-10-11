e41da91f467f929f8a323437d945706faa8db925
svasenkov <aanher@gmail.com>
2022-10-11 20:38:15 +0300

    added pojo-models

---
+++ src/test/java/models/LoginResponsePojoModel.java
@@ -0,0 +1,16 @@
+package models;
+
+public class LoginResponsePojoModel {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+
+//    "{ \"token\": \"QpwL5tke4Pnpja7X4\" }"
+}
